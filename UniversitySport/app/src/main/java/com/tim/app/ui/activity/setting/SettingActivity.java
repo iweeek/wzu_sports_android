9da3c050f4265e2ce213da89db21b9f8119c76ac
chaiyu <chaiyu2002@gmail.com>
2017-08-05 10:19:02 +0800

    修改了【检查更新】的点击行为

--- UniversitySport/app/src/main/java/com/tim/app/ui/activity/setting/SettingActivity.java
+++ UniversitySport/app/src/main/java/com/tim/app/ui/activity/setting/SettingActivity.java
@@ -1,18 +1,28 @@
 package com.tim.app.ui.activity.setting;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.application.library.net.JsonResponseCallback;
 import com.application.library.util.PackageUtil;
 import com.tim.app.R;
 import com.tim.app.constant.AppConstant;
+import com.tim.app.server.api.ServerInterface;
 import com.tim.app.ui.activity.AboutActivity;
 import com.tim.app.ui.activity.BaseActivity;
 import com.tim.app.ui.activity.SportFixedLocationActivity;
+import com.tim.app.util.DownloadAppUtils;
+
+import org.json.JSONObject;
 
 import java.util.Random;
 
@@ -20,12 +30,14 @@ public class SettingActivity extends BaseActivity {
 
     private static final String TAG = "SettingActivity";
 
-    private TextView tvVersionCode;
+    private TextView tvVersionName;
 
     private RelativeLayout rlModtifyPassword;
     private RelativeLayout rlAboutUS;
     private RelativeLayout rlCheckUpdate;
     private RelativeLayout rlTest;
+
+    public static String noNewVersionMsg = "没有发现新版本";
 
     /**
      * 启动设置界面的统一接口
@@ -50,7 +62,8 @@ public class SettingActivity extends BaseActivity {
         rlCheckUpdate = (RelativeLayout) findViewById(R.id.rlCheckUpdate);
         rlTest = (RelativeLayout) findViewById(R.id.rlTest);
 
-        tvVersionCode = (TextView) findViewById(R.id.tvVersionCode);
+        tvVersionName = (TextView) findViewById(R.id.tvVersionName);
+
         findViewById(R.id.ibBack).setOnClickListener(this);
         rlModtifyPassword.setOnClickListener(this);
         rlAboutUS.setOnClickListener(this);
@@ -61,7 +74,7 @@ public class SettingActivity extends BaseActivity {
 
     @Override
     public void initData() {
-        tvVersionCode.setText("当前版本：" + PackageUtil.getVersionName(this));
+        tvVersionName.setText("当前版本：" + PackageUtil.getVersionName(this));
     }
 
 
@@ -79,6 +92,81 @@ public class SettingActivity extends BaseActivity {
             Intent aboutIntent = new Intent(this, AboutActivity.class);
             startActivity(aboutIntent);
         } else if (v.getId() == R.id.rlCheckUpdate) {
+            ServerInterface.instance().queryAppVersion(new JsonResponseCallback() {
+                @Override
+                public boolean onJsonResponse(JSONObject json, int errCode, String errMsg, int id, boolean fromCache) {
+//                String versionName = "";
+//                int versionCode;
+//                String changeLog = "";
+//                String apkUrl = "";
+//                boolean isForced = false;
+
+                    if (errCode == 0) {
+                        try {
+                            final String versionName = json.getJSONObject("data").getJSONObject("latestAndroidVerisonInfo").getString("versionName");
+                            final int versionCode = json.getJSONObject("data").getJSONObject("latestAndroidVerisonInfo").getInt("versionCode");
+                            final String changeLog = json.getJSONObject("data").getJSONObject("latestAndroidVerisonInfo").getString("changeLog");
+                            final String apkUrl = json.getJSONObject("data").getJSONObject("latestAndroidVerisonInfo").getString("apkUrl");
+                            final boolean isForced = json.getJSONObject("data").getJSONObject("latestAndroidVerisonInfo").getBoolean("isForced");
+
+                            PackageManager manager = (SettingActivity.this).getPackageManager();
+                            PackageInfo info = manager.getPackageInfo(SettingActivity.this.getPackageName(), 0);
+
+                            final AlertDialog.Builder builder =
+                                    new AlertDialog.Builder(SettingActivity.this);
+                            AlertDialog dialog;
+//                                normalDialog.setIcon(R.drawable.icon_dialog);
+                            builder.setTitle("版本升级");
+                            builder.setPositiveButton("确认",
+                                    new DialogInterface.OnClickListener() {
+
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            DownloadAppUtils.downloadForAutoInstall(SettingActivity.this, apkUrl, "下载新版本");
+                                            if (isForced) {
+                                                //无操作
+                                            } else {
+                                                return;
+                                            }
+                                        }
+                                    });
+                            builder.setMessage("发现新版本");
+                            if (versionCode > info.versionCode) {
+                                if (isForced) {
+                                    //对话框不变化
+                                } else {
+                                    builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.dismiss();
+                                        }
+                                    });
+
+                                }
+
+                                dialog = builder.create();
+                                dialog.show();
+
+                            } else {
+                                Toast.makeText(SettingActivity.this, noNewVersionMsg, Toast.LENGTH_SHORT).show();
+                            }
+
+                            return true;
+                            //发生以下情况的可能性正常时，是不存在的，所以这里不处理
+                        } catch (org.json.JSONException e) {
+                            e.printStackTrace();
+                            return false;
+                        } catch (PackageManager.NameNotFoundException e) {
+                            e.printStackTrace();
+                            return false;
+                        }
+
+                    } else {
+                        // TODO 网络出现问题？该接口出现问题？
+                        return false;
+                    }
+                }
+            });
 
         } else if (v.getId() == R.id.rlTest) {
             boolean value = new Random().nextBoolean();
