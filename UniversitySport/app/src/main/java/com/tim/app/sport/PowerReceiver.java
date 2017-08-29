591988a28891acbed9c86330f7770535e101a1e0
SmartNi <nijun717@gmail.com>
2017-08-29 15:24:35 +0800

    see #844_3

--- UniversitySport/app/src/main/java/com/tim/app/sport/PowerReceiver.java
+++ UniversitySport/app/src/main/java/com/tim/app/sport/PowerReceiver.java
@@ -20,7 +20,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.BatteryManager;
 import android.util.Log;
 
 public class PowerReceiver extends BroadcastReceiver {
@@ -45,14 +44,5 @@ public class PowerReceiver extends BroadcastReceiver {
             context.startService(new Intent(context, SensorService.class)
                     .putExtra("action", SensorService.ACTION_PAUSE));
         }
-
-
-        int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
-                status == BatteryManager.BATTERY_STATUS_FULL;
-
-        int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
-        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
-        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
     }
 }
