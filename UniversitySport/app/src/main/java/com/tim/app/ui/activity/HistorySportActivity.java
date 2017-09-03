bada2fe16965d8b4273c6dc9ec18adae8586ac6c
SmartNi <nijun717@gmail.com>
2017-09-03 10:51:04 +0800

    see #871_1

--- UniversitySport/app/src/main/java/com/tim/app/ui/activity/HistorySportActivity.java
+++ UniversitySport/app/src/main/java/com/tim/app/ui/activity/HistorySportActivity.java
@@ -1,11 +1,11 @@
 package com.tim.app.ui.activity;
 
 import android.content.res.Configuration;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
 import com.application.library.util.SmoothSwitchScreenUtil;
-import com.application.library.widget.smarttab.SmartTabLayout;
 import com.tim.app.R;
 import com.tim.app.constant.AppConstant;
 import com.tim.app.ui.adapter.TabAdapter;
@@ -18,7 +18,7 @@ public class HistorySportActivity extends ToolbarActivity {
 
     private static final String TAG = "HistorySportActivity";
 
-    private SmartTabLayout stbNavBar;
+    private TabLayout tabLayout;
     private ViewPager vpHistoryData;
     private TabAdapter pagerAdapter;
 
@@ -30,20 +30,18 @@ public class HistorySportActivity extends ToolbarActivity {
 
     @Override
     public void initView() {
-        stbNavBar = (SmartTabLayout) findViewById(R.id.stbNavBar);
+        tabLayout = (TabLayout) findViewById(R.id.stbNavBar);
         vpHistoryData = (ViewPager)findViewById(R.id.vpHistoryData);
 
         pagerAdapter = new TabAdapter(getSupportFragmentManager());
         pagerAdapter.addFragment(HistoryDataFragment.newInstance(AppConstant.TYPE_WEEK), "本周");
         pagerAdapter.addFragment(HistoryDataFragment.newInstance(AppConstant.TYPE_MONTH), "本月");
         pagerAdapter.addFragment(HistoryDataFragment.newInstance(AppConstant.TYPE_TERM), "本学期");
-//        pagerAdapter.addFragment(HistoryDataFragment.newInstance(AppConstant.TYPE_HISTORY), "历史");
         vpHistoryData.setOffscreenPageLimit(2);//除去当前显示页面外，还需要预先加载的页面个数
         vpHistoryData.setAdapter(pagerAdapter);
         vpHistoryData.setCurrentItem(0);
-        stbNavBar.setViewPager(vpHistoryData);
+        tabLayout.setupWithViewPager(vpHistoryData);
     }
-
 
     @Override
     public void initData() {
@@ -74,7 +72,6 @@ public class HistorySportActivity extends ToolbarActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
     }
-
 
     @Override
     protected int getLayoutId() {
