ba6573a7459410fc0c177b6ab24031c0b5cbcab7
SmartNi <nijun717@gmail.com>
2017-10-10 15:15:34 +0800

    see f_1043_1

--- UniversitySport/app/src/main/java/com/tim/app/ui/activity/setting/SettingActivity.java
+++ UniversitySport/app/src/main/java/com/tim/app/ui/activity/setting/SettingActivity.java
@@ -69,35 +69,39 @@ public class SettingActivity extends ToolbarActivity {
                 public boolean onJsonResponse(JSONObject json, int errCode, String errMsg, int id, boolean fromCache) {
                     if (errCode == 0) {
                         try {
-                            final String versionName = json.getJSONObject("data").getJSONObject("latestVerison").getString("versionName");
-                            final int versionCode = json.getJSONObject("data").getJSONObject("latestVerison").getInt("versionCode");
-                            final String changeLog = json.getJSONObject("data").getJSONObject("latestVerison").getString("changeLog");
-                            final String apkUrl = json.getJSONObject("data").getJSONObject("latestVerison").getString("apkUrl");
-                            final boolean isForced = json.getJSONObject("data").getJSONObject("latestVerison").getBoolean("isForced");
+                            JSONObject latestVersion = json.getJSONObject("data").getJSONObject("latestVerison");
+                            final String versionName = latestVersion.getString("versionName");
+                            final int versionCode = latestVersion.getInt("versionCode");
+                            final String changeLog = latestVersion.getString("changeLog");
+                            final String apkUrl = latestVersion.getString("downloadUrl");
+                            final boolean isForced = latestVersion.getBoolean("isForced");
 
                             PackageManager manager = (SettingActivity.this).getPackageManager();
                             PackageInfo info = manager.getPackageInfo(SettingActivity.this.getPackageName(), 0);
 
-                            final AlertDialog.Builder builder =
-                                    new AlertDialog.Builder(SettingActivity.this);
-                            AlertDialog dialog;
-                            // normalDialog.setIcon(R.drawable.icon_dialog);
-                            builder.setTitle("版本升级");
-                            builder.setPositiveButton("确认",
-                                    new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            DownloadAppUtils.downloadForAutoInstall(SettingActivity.this, apkUrl, "下载新版本");
-                                            if (isForced) {
-                                                //无操作
-                                            } else {
-                                                return;
-                                            }
-                                        }
-                                    });
-                            builder.setMessage("发现新版本");
                             if (versionCode > info.versionCode) {
+
+                                final AlertDialog.Builder builder =
+                                        new AlertDialog.Builder(SettingActivity.this);
+                                AlertDialog dialog;
+                                builder.setTitle("版本升级");
+                                builder.setPositiveButton("确认",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                DownloadAppUtils.downloadForAutoInstall(SettingActivity.this, apkUrl, "下载新版本");
+                                                //
+                                                // if (NetUtil.isWifi(SettingActivity.this)) {
+                                                //     DownloadAppUtils.downloadForAutoInstall(SettingActivity.this, apkUrl, "下载新版本");
+                                                // } else if (NetUtil.isMobile(SettingActivity.this)) {
+                                                //
+                                                // }
+                                            }
+                                        });
+                                builder.setMessage(changeLog.replace("\\n", " \n"));
+
                                 if (isForced) {
+                                    builder.setCancelable(false);
                                     //对话框不变化
                                 } else {
                                     builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
@@ -114,7 +118,6 @@ public class SettingActivity extends ToolbarActivity {
                                 Toast.makeText(SettingActivity.this, getString(R.string.prompt_no_update), Toast.LENGTH_SHORT).show();
                             }
                             return true;
-                            //发生以下情况的可能性正常时，是不存在的，所以这里不处理
                         } catch (org.json.JSONException e) {
                             e.printStackTrace();
                             return false;
---
+++ UniversitySport/app/src/main/java/com/tim/app/util/NetUtil.java
@@ -0,0 +1,41 @@
+package com.tim.app.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * @创建者 倪军
+ * @创建时间 10/10/2017
+ * @描述
+ */
+
+public class NetUtil {
+
+    public static boolean isConnected(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+        return isConnected;
+    }
+
+    public static boolean isWifi(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        boolean isWifi = activeNetwork.getType() == ConnectivityManager.TYPE_WIFI;
+        return isWifi;
+    }
+
+    public static boolean isMobile(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        boolean isMobile = activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE;
+        return isMobile;
+    }
+}
